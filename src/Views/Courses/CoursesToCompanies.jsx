73e0aac08a08b48b0dff14e1fd1979019024aed5
Juan Carlos <j.cruzb@outlook.com>
2024-01-10 17:44:41 +0100

    update

--- src/Views/Courses/CoursesToCompanies.jsx
+++ src/Views/Courses/CoursesToCompanies.jsx
@@ -1,36 +1,48 @@
-import { Avatar, Box, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import { getCourseDetail } from '../../Services/CoursesService';
-import { getClientsList } from '../../Services/ClientsService';
+import {
+  Avatar,
+  Box,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getCourseDetail } from "../../Services/CoursesService";
+import { getClientsList } from "../../Services/ClientsService";
+
 
 function DataTable({ rows, columns }) {
+  const [selectionModel, setSelectionModel] = useState([]);
+
+  useEffect(() => {
+    console.log(selectionModel);
+  }, [selectionModel]);
 
 
-    const [selectionModel, setSelectionModel] = useState([]);
 
-    const handleChange = (e) => {
-        if(e.field === '__check__') {
+  useEffect(() => {
+    const selectedRows = rows.filter((row) => row.selection).map((row) => row.id);
+    setSelectionModel(selectedRows);
+  }, [rows]);
 
-        console.log(e.value);
-        console.log(e.row);
-        }
-    }
-    
+  const handleChange = (newSelection) => {
+    setSelectionModel(newSelection.selectionModel);
+  };
+
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: 400, width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
         pageSize={5}
         checkboxSelection
+        selectionModel={selectionModel}
         onCellClick={(e) => {
-            handleChange(e);
-        }
-        }
-
-
+          handleChange(e);
+        }}
       />
     </div>
   );
@@ -63,33 +75,47 @@ const CoursesToCompanies = () => {
   }, []);
 
   const columns = [
-    { field: 'nif', headerName: 'NIF', width: 120 },
-    { field: 'name', headerName: 'Nombre', width: 250 },
-    { field: 'email', headerName: 'Email', width: 200 },
+    { field: "nif", headerName: "NIF", width: 120 },
+    { field: "name", headerName: "Nombre", width: 250 },
+    { field: "email", headerName: "Email", width: 200 },
   ];
 
+ 
 
-  console.log(selectedCompanies);
+    companies.forEach((company) => {
+        company.selection = false;
+        if (company.courses) {
+            company.courses.forEach((course) => {
+            if (course.id === id) {
+                company.selection = true;
+            }
+            });
+        }
+        });
 
-  return (
-    <Box>
-      <Typography variant="h3" sx={{ marginBottom: 3 }}>
-        Empresas asignadas al curso
-      </Typography>
-      <List>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar src={course.mainImage} />
-          </ListItemAvatar>
-          <ListItemText primary={course.name} />
-        </ListItem>
-      </List>
 
-      <Box sx={{ margin: 2 }}>
-        <DataTable rows={companies} columns={columns} />
+  if (course === undefined) {
+    return <div>loading...</div>;
+  } else
+    return (
+      <Box>
+        <Typography variant="h3" sx={{ marginBottom: 3 }}>
+          Empresas asignadas al curso
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar src={course.mainImage} />
+            </ListItemAvatar>
+            <ListItemText primary={course.name} />
+          </ListItem>
+        </List>
+
+        <Box sx={{ margin: 2 }}>
+          <DataTable rows={companies} columns={columns} />
+        </Box>
       </Box>
-    </Box>
-  );
+    );
 };
 
 export default CoursesToCompanies;
