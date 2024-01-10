a1440be011949779a10b32d985979c3431ca63eb
Juan Cruz <j.cruzb@outlook.com>
2024-01-10 21:33:54 +0100

    update

--- src/Views/Courses/CoursesToCompanies.jsx
+++ src/Views/Courses/CoursesToCompanies.jsx
@@ -1,121 +1,86 @@
 import {
-  Avatar,
-  Box,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
-  Typography,
+    Avatar,
+    Box,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Typography,
 } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { getCourseDetail } from "../../Services/CoursesService";
 import { getClientsList } from "../../Services/ClientsService";
+import AsignCourseTable from "../../Components/AsignCourseTable/AsignCourseTable";
 
-
-function DataTable({ rows, columns }) {
-  const [selectionModel, setSelectionModel] = useState([]);
-
-  useEffect(() => {
-    console.log(selectionModel);
-  }, [selectionModel]);
-
-
-
-  useEffect(() => {
-    const selectedRows = rows.filter((row) => row.selection).map((row) => row.id);
-    setSelectionModel(selectedRows);
-  }, [rows]);
-
-  const handleChange = (newSelection) => {
-    setSelectionModel(newSelection.selectionModel);
-  };
-
-  return (
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        checkboxSelection
-        selectionModel={selectionModel}
-        onCellClick={(e) => {
-          handleChange(e);
-        }}
-      />
-    </div>
-  );
-}
 
 const CoursesToCompanies = () => {
-  const { id } = useParams();
-  const [course, setCourse] = useState({});
-  const [companies, setCompanies] = useState([]);
-  const [selectedCompanies, setSelectedCompanies] = useState([]);
+    const { id } = useParams();
+    const [course, setCourse] = useState({});
+    const [companies, setCompanies] = useState([]);
 
-  useEffect(() => {
-    getCourseDetail(id)
-      .then((data) => {
-        setCourse(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [id]);
+    useEffect(() => {
+        getCourseDetail(id)
+            .then((data) => {
+                setCourse(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [id]);
 
-  useEffect(() => {
-    getClientsList()
-      .then((data) => {
-        setCompanies(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  const columns = [
-    { field: "nif", headerName: "NIF", width: 120 },
-    { field: "name", headerName: "Nombre", width: 250 },
-    { field: "email", headerName: "Email", width: 200 },
-  ];
-
- 
+    useEffect(() => {
+        getClientsList()
+            .then((data) => {
+                setCompanies(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     companies.forEach((company) => {
         company.selection = false;
         if (company.courses) {
             company.courses.forEach((course) => {
-            if (course.id === id) {
-                company.selection = true;
-            }
+                if (course.id === id) {
+                    company.selection = true;
+                }
             });
         }
-        });
+    });
+
+    const columns = ["Seleccionar", "Nombre", "Email", "Telefono"];
+    const properties = [ "name", "email", "phone"];
 
 
-  if (course === undefined) {
-    return <div>loading...</div>;
-  } else
-    return (
-      <Box>
-        <Typography variant="h3" sx={{ marginBottom: 3 }}>
-          Empresas asignadas al curso
-        </Typography>
-        <List>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar src={course.mainImage} />
-            </ListItemAvatar>
-            <ListItemText primary={course.name} />
-          </ListItem>
-        </List>
+    if (course === undefined) {
+        return <div>loading...</div>;
+    } else
+        return (
+            <Box>
+                <Typography variant="h3" sx={{ marginBottom: 3 }}>
+                    Empresas asignadas al curso
+                </Typography>
+                <List>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar src={course.mainImage} />
+                        </ListItemAvatar>
+                        <ListItemText primary={course.name} />
+                    </ListItem>
+                </List>
 
-        <Box sx={{ margin: 2 }}>
-          <DataTable rows={companies} columns={columns} />
-        </Box>
-      </Box>
-    );
+                <Box sx={{ margin: 2 }}>
+                    <AsignCourseTable
+                    rows={companies}
+                    columns={columns}
+                    properties={properties}
+
+                    ></AsignCourseTable>
+                </Box>
+            </Box>
+        );
 };
 
 export default CoursesToCompanies;
