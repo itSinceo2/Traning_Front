5de8a9e65fb4784bcfdede3c57db12bd17b1f3a9
Juan Carlos <j.cruzb@outlook.com>
2024-01-10 16:34:22 +0100

    update

--- src/Views/Courses/CoursesToCompanies.jsx
+++ src/Views/Courses/CoursesToCompanies.jsx
@@ -5,91 +5,91 @@ import { useParams } from 'react-router';
 import { getCourseDetail } from '../../Services/CoursesService';
 import { getClientsList } from '../../Services/ClientsService';
 
+function DataTable({ rows, columns }) {
 
+
+    const [selectionModel, setSelectionModel] = useState([]);
+
+    const handleChange = (e) => {
+        if(e.field === '__check__') {
+
+        console.log(e.value);
+        console.log(e.row);
+        }
+    }
+    
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        checkboxSelection
+        onCellClick={(e) => {
+            handleChange(e);
+        }
+        }
+
+
+      />
+    </div>
+  );
+}
 
 const CoursesToCompanies = () => {
+  const { id } = useParams();
+  const [course, setCourse] = useState({});
+  const [companies, setCompanies] = useState([]);
+  const [selectedCompanies, setSelectedCompanies] = useState([]);
 
-    const { id } = useParams();
-    const [course, setCourse] = useState({});
-    const [companies, setCompanies] = useState([]);
+  useEffect(() => {
+    getCourseDetail(id)
+      .then((data) => {
+        setCourse(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
 
-    useEffect(() => {
-        getCourseDetail(id)
-            .then((data) => {
-                setCourse(data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, [id])
+  useEffect(() => {
+    getClientsList()
+      .then((data) => {
+        setCompanies(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    useEffect(() => {
-        getClientsList()
-            .then((data) => {
-                setCompanies(data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
-    , [])
-
-    console.log(companies);
-
-    const columns = [
-        { field: 'nif', headerName: 'NIF', width: 120 },
-        { field: 'name', headerName: 'Nombre', width: 250 },
-        { field: 'email', headerName: 'Email', width: 200 },
-    ];
-    
-    const rows = [...companies
-    ];
+  const columns = [
+    { field: 'nif', headerName: 'NIF', width: 120 },
+    { field: 'name', headerName: 'Nombre', width: 250 },
+    { field: 'email', headerName: 'Email', width: 200 },
+  ];
 
 
-    return (
-        <Box>
-            <Typography variant="h3" sx={{ marginBottom: 3 }}>Empresas asignadas al curso</Typography>
-            <List>
-                <ListItem>
-                <ListItemAvatar>
-                    <Avatar src={course.mainImage}/>
-                </ListItemAvatar>
-                <ListItemText primary={course.name}/>
-                </ListItem>
+  console.log(selectedCompanies);
 
-            </List>
-            
-            <Box sx={{ margin: 2 }}>
-            <DataTable 
-            rows={rows}
-            columns={columns}
-            />
-            </Box>
-        </Box>
-    )
-}
+  return (
+    <Box>
+      <Typography variant="h3" sx={{ marginBottom: 3 }}>
+        Empresas asignadas al curso
+      </Typography>
+      <List>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar src={course.mainImage} />
+          </ListItemAvatar>
+          <ListItemText primary={course.name} />
+        </ListItem>
+      </List>
 
-export default CoursesToCompanies
+      <Box sx={{ margin: 2 }}>
+        <DataTable rows={companies} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
 
-
-
-
-
-
-function DataTable({ rows, columns}) {
-    return (
-        <div style={{ height: 400, width: '100%' }}>
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                initialState={{
-                    pagination: {
-                        paginationModel: { page: 0, pageSize: 5 },
-                    },
-                }}
-                pageSizeOptions={[5, 10]}
-                checkboxSelection
-            />
-        </div>
-    );
-}
+export default CoursesToCompanies;
