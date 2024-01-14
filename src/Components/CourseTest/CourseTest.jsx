97ebc743d043d990e0022240b74fc2250b2cb17d
Juan Cruz <j.cruzb@outlook.com>
2024-01-14 15:52:54 +0100

    test

---
+++ src/Components/CourseTest/CourseTest.jsx
@@ -0,0 +1,50 @@
+import { Box, Button, Card, Checkbox, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
+
+const CourseTest = ({
+    contentArray,
+    page,
+    handleToggle,
+    selectedOptions
+}) => {
+    return(
+        <Card sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginY: 2 }}>
+                            <Typography>{contentArray[page - 1].title}</Typography>
+                            <form>
+                                <List dense sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                                    {contentArray[page - 1].questions.map((question) => {
+                                        const labelId = `checkbox-list-secondary-label-${question._id}`;
+                                        return (
+                                            <Box key={question._id}>
+                                                <Typography variant='h5'>{question.question}</Typography>
+                                                {question.options.map((option) => (
+                                                    <Box key={option._id}>
+                                                        <ListItem
+                                                            key={option._id}
+                                                            secondaryAction={
+                                                                <Checkbox
+                                                                    edge="end"
+                                                                    onChange={handleToggle(question._id, option._id)}
+                                                                    checked={(selectedOptions[question._id] || []).indexOf(option._id) !== -1}
+                                                                    inputProps={{ 'aria-labelledby': labelId }}
+                                                                />
+                                                            }
+                                                            disablePadding
+                                                        >
+                                                            <ListItemButton role={undefined} onClick={handleToggle(question._id, option._id)} dense>
+                                                                <ListItemText id={option._id} primary={`${option.option}`} />
+                                                            </ListItemButton>
+                                                        </ListItem>
+                                                    </Box>
+                                                ))}
+                                            </Box>
+                                        );
+                                    })}
+                                </List>
+                                <Button variant="contained" color="primary" type="submit" sx={{ marginBottom: 2 }}>Enviar</Button>
+                            </form>
+
+                        </Card>
+    )
+}
+
+export default CourseTest;
--- src/Views/Courses/ViewOfContent.jsx
+++ src/Views/Courses/ViewOfContent.jsx
@@ -1,40 +1,75 @@
-
 import Typography from '@mui/material/Typography';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Box, Card } from '@mui/material';
+import CourseTest from '../../Components/CourseTest/CourseTest';
 
 const ViewOfContent = ({ content, test }) => {
-
-
-    console.log(test)
     const [page, setPage] = useState(1);
+    const [contentArray, setContentArray] = useState([]);
+    const [selectedOptions, setSelectedOptions] = useState({});
+
     const handleChange = (event, value) => {
         setPage(value);
     };
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const newArray = [];
+                content.forEach((contentItem) => {
+                    const matchingTest = test.find((testItem) => contentItem.title === testItem.title);
+                    if (matchingTest) {
+                        newArray.push(contentItem)
+                        newArray.push(matchingTest);
+                    } else {
+                        newArray.push(contentItem);
+                    }
+                });
+                setContentArray(newArray);
+            } catch (error) {
+                console.error(error);
+            }
+        };
 
-    if (!content) {
+        fetchData();
+    }, [content, test]);
+
+    const handleToggle = (questionId, optionId) => () => {
+        setSelectedOptions((prevSelected) => ({
+            ...prevSelected,
+            [questionId]: optionId,
+        }));
+    };
+
+    if (!contentArray || contentArray.length === 0) {
         return (
             <div>
                 <h1>No hay contenido o está cargando</h1>
                 <p>Si el problema persiste contacte con su administrador</p>
             </div>
-        )
+        );
     } else {
-
         return (
             <Stack spacing={2}>
-
                 <Box>
-                    <Card sx={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
-                    <Typography>{content[page - 1].title}</Typography>
-                    <img src={content[page - 1].image} alt={content[page - 1].name} />
-                    <Typography>{content[page - 1].description}</Typography>
-                    </Card>
+                    {contentArray[page - 1].questions ? (
+                        <CourseTest
+                            contentArray={contentArray}
+                            page={page}
+                            handleToggle={handleToggle}
+                            selectedOptions={selectedOptions}
+                        />
+                    ) : (
+                        <Card sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginY: 2 }}>
+                            <Typography>{contentArray[page - 1].title}</Typography>
+                            <img src={contentArray[page - 1].image} alt={contentArray[page - 1].name} />
+                            <Typography>{contentArray[page - 1].description}</Typography>
+                        </Card>
+                    )}
                 </Box>
-                <Pagination count={content.length} page={page} onChange={handleChange} />
+                <Pagination count={contentArray.length} page={page} onChange={handleChange} />
             </Stack>
         );
     }
