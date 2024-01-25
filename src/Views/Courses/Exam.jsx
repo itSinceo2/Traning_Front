475b61ecc07e36e79b204ca2e206aaf689ef9488
Juan Cruz <j.cruzb@outlook.com>
2024-01-25 07:37:50 +0100

    update

--- src/Views/Courses/Exam.jsx
+++ src/Views/Courses/Exam.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import { getCourseDetail } from "../../Services/CoursesService"
+import { Avatar, Box, Divider, Typography } from "@mui/material"
 
 const Exam = () => {
 
@@ -11,21 +12,30 @@ const Exam = () => {
     useEffect(() => {
         getCourseDetail(id)
             .then((response) => {
-                console.log(response)
                 setCourse(response)
-                setExam(response.exam)
+                setExam(response.Exam)
             }).catch((error) => {
                 console.log(error);
             });
     }, [id])
-
+    console.log(course);
     console.log(exam);
 
-    return (
-        <div>
-            <h1>Exam</h1>
-        </div>
-    )
+    if (!course || !exam) {
+        return <h1>Loading...</h1>
+    } else {
+        return (
+            <Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }} src={course.mainImage} />
+                    <Typography variant="h4" gutterBottom component="div">{course.name}</Typography>
+                </Box>
+                <Divider />
+                
+
+            </Box>
+        )
+    }
 }
 
 export default Exam
